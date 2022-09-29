d6f293ab83dac00ac9c2d37ad0e5fd9727a24509
zhoan1 <73973909+zhoan1@users.noreply.github.com>
2022-09-29 16:26:56 -0400

    Initial commit

---
+++ .devcontainer/prefs.js
@@ -0,0 +1,7 @@
+// disable update-related features
+user_pref('app.normandy.enabled', false);
+user_pref('app.update.auto', false);
+user_pref('app.update.download.promptMaxAttempts', 0);
+user_pref('app.update.elevation.promptMaxAttempts', 0);
+
+user_pref('browser.open.lastDir', '/workspaces/lab2');
